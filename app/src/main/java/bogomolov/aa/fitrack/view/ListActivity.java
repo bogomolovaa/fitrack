65c0f20854554c8ebb24bab0bb2d59924c83e3dd
BogomolovAA <alllexander@gmail.com>
2019-10-16 16:20:05 +0300

    Initial commit

---
+++ app/src/main/java/bogomolov/aa/fitrack/view/ListActivity.java
@@ -0,0 +1,16 @@
+package bogomolov.aa.fitrack.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import bogomolov.aa.fitrack.R;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
