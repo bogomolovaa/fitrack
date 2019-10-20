3bbf069b08df960e90026de27544175edf45b069
Александр <alllexander@gmail.com>
2019-10-21 00:20:58 +0300

    .

--- app/src/main/java/bogomolov/aa/fitrack/presenter/MainPresenter.java
+++ app/src/main/java/bogomolov/aa/fitrack/presenter/MainPresenter.java
@@ -1,10 +1,7 @@
 package bogomolov.aa.fitrack.presenter;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 
-import bogomolov.aa.fitrack.model.TrackerService;
 import bogomolov.aa.fitrack.view.MainView;
 
 public class MainPresenter {
@@ -16,7 +13,7 @@ public class MainPresenter {
 
     public void startService(){
         Context context = (Context)mainView;
-        Intent intent =  new Intent(context, TrackerService.class);
-        context.startService(intent);
+        //Intent intent =  new Intent(context, TrackerService.class);
+        //context.startService(intent);
     }
 }
